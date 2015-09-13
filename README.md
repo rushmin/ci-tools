78c555e9cf77b7c52cded4242ae92bdf3f82a74c
Rushmin Fernando <rushmin@wso2.com>
2015-09-13 12:55:27 +0530

    Getting rid of repo info section in the settings file. Repo info are fetched by calling GitHub API.

--- README.md
+++ README.md
@@ -1,37 +1,62 @@
 # ci-tools
 
 ####What ?
-Some CI extensions for Git and Maven
+Some CI extensions for Git and Maven. Only supports merging GitHub pull requests as of now.
 
-####How ?
+####Requirements
+Python 2.7.5+
+
+####Usage
 
 ```
-ci-tools.py mergePullRequest "{'repo':'ci-tools-test', 'id':'1'}" --push
+ci-tools.py [-h] [--settings SETTINGS] mergePullRequest [-h] [--push] pullRequest
+
+positional arguments:
+
+  pullRequest  e.g. => "{'repoLocation':'ci-tools-test','pullRequestUrl':'https://github.com/john/ci-tools-test/pull/1'}"
+
+optional arguments:
+  -h, --help   show the message and exit
+  --settings   settings file location
+  --push       flag to push the merge to the origin
 ```
 
-A data file which contains the repo information should be there in the current directory. The name of the file should be **ci-tools-data.json**
+####Settings
 
-#####Sample file
+ci-tools needs a settings file which contains settings to access git providers like GitHub. (See the sample settings file below)
+
+**NOTE : ** You only have to change the username and password in this setting file, unless the git providers has changed their HTTP resource URLs.
+
+
+The settings file can be specified using the **_--settings_** argument.
+
+If the file is not specified, ci-tools searches for a local settings file named **_ci-tools-settings.json_** in the **_current directory_**
+
+If ci-tools can't find such file, it tries to load the default setting file located in **_HOME/.ci-tools/ci-tools-settings.json_**
+
+So make sure a settings file is located at least in of these locations.
+
+#####Sample settings file
 
 ```json
 {
-  "repositories": {
-    "ci-tools-test": {
-      "name": "ci-tools-test",
-      "cloneLocation": "/wso2dev/rnd/poc/wso2-ci-tools/builder/ci-tools-test",
-      "github": {
-        "url": "https://github.com/sumuditha-viraj/ci-tools-test",
-        "owner": "sumuditha-viraj"
-      }
-    }
-  },
-  "conf": {
-    "github": {
-      "username": "rushmin",
-      "password": "Password",
-      "api": {
-        "root": "https://api.github.com",
+  "github": {
+    "username": "your_username",
+    "password": "your_password",
+    "api": {
+      "root": "https://api.github.com",
+      "urls": {
         "getPullRequest": "/repos/{}/{}/pulls/{}"
+      }
+    },
+    "urlPatternsRegEx": {
+      "pullRequest": {
+        "pattern": "https://github.com/(.+)/(.+)/pull/([0-9]+)",
+        "groups": {
+          "owner": 1,
+          "repo": 2,
+          "pullRequestId": 3
+        }
       }
     }
   }
--- ci-tools.py
+++ ci-tools.py
@@ -1,3 +1,5 @@
+#Version : 0.1.0
+
 import os
 import sys
 import time
@@ -5,30 +7,44 @@ from subprocess import call
 import argparse
 import requests
 import json
+import re
 
 class GitEngine:
 
-    def __init__(self, repos, conf):
-        self.repos = repos
-        self.conf = conf
+    def __init__(self, settings):
+        self.settings = settings
 
     def mergePullRequest(self, pullRequestInfo, shouldPush):
 
-        print pullRequestInfo
-        repoName = pullRequestInfo['repo']
-        repoLocation = self.repos[repoName]['cloneLocation']
-        repoOwner = self.repos[repoName]['github']['owner']
-        pullRequestId = pullRequestInfo['id']
+        localRepoLocation = pullRequestInfo['repoLocation']
+        pullRequestUrl = pullRequestInfo['pullRequestUrl']
 
-        print "Merging pull requeust. repo : '{}', clone : '{}', repo-owner : '{}', pull-request-id : '{}'".format(repoName, repoLocation, repoOwner, pullRequestId)
+        print "\nProcessing pull request '{}' on '{}'".format(pullRequestUrl, localRepoLocation)
 
-        os.chdir(repoLocation)
-        print "Switched to git repo '{}'".format(repoLocation)
+        # Change the directory to the repo location
+        os.chdir(localRepoLocation)
+        print "\nSwitched to git repo '{}'".format(os.getcwd())
+
+        # Parse the pull request url to get the repo info and the pull request id.
+        parsedPullRequest = self.parsePullRequestUrl(pullRequestUrl)
+        repoName = parsedPullRequest['repo']
+        repoOwner = parsedPullRequest['owner']
+        pullRequestId =parsedPullRequest['pullRequestId']
 
         # Fetch pull request info from github
-        pullRequestInfoApiUrl = self.conf['github']['api']['root'] + self.conf['github']['api']['getPullRequest'].format(repoOwner, repoName, pullRequestId)
+        pullRequestInfoApiUrl = self.settings['github']['api']['root'] + self.settings['github']['api']['urls']['getPullRequest'].format(repoOwner, repoName, pullRequestId)
         response =  self.invokeGitHubApi(pullRequestInfoApiUrl)
         pullRequestInfo = response.json();
+
+        if self.shouldContinue(pullRequestInfo) is False:
+            exit()
+
+        validationResult = self.validatePullRequest(pullRequestInfo)
+
+        if validationResult['status'] is False:
+            print '\nError : Cannot merge the pull requet. Reason : ' + validationResult['reason']
+
+        print "\nMerging pull requeust. repo : '{}', repo-owner : '{}', pull-request-id : '{}'".format(repoName, repoOwner, pullRequestId)
 
         # Fetch from the origin
         self.git('fetch')
@@ -51,13 +67,40 @@ class GitEngine:
         self.git('pull', [pullRequestRepoUrl, pullRequestBranch])
 
         # Merge the pull request
-        mergeMessage = 'Merge pull request #{} from {}/{}'.format(pullRequestId, pullRequestCreator, pullRequestBranch)
+        mergeMessage = '"Merge pull request #{} from {}/{}"'.format(pullRequestId, pullRequestCreator, pullRequestBranch)
         self.git('checkout', [baseBranch])
         self.git('merge', ['--no-ff', '-m', mergeMessage, tempBranchName])
 
         # Push the changes
         if shouldPush:
             self.git('push', ['origin', baseBranch])
+
+    def shouldContinue(self, pullRequestInfo):
+
+        message = '\nDo you want to merge the following pull request ?'
+
+        baseRepo = pullRequestInfo['base']['repo']['full_name']
+        baseBranch = pullRequestInfo['base']['ref']
+        creator = pullRequestInfo['user']['login']
+        title = pullRequestInfo['title']
+        updatedAt = pullRequestInfo['updated_at']
+        pullRequestBranch = pullRequestInfo['head']['ref']
+
+        message = message + '\n\tbase-repo = {}\n\tbase-branch = {}\n\tcreator = {}\n\ttitle = {}\n\tupdated-at = {}\n\tpull-request-branch = {}\n\n ==>  [Yes/no] - '.format(baseRepo, baseBranch, creator, title, updatedAt, pullRequestBranch)
+
+        response = raw_input(message);
+
+        if response == 'Yes':
+            return True
+        else:
+            return False
+
+    def validatePullRequest(self, pullRequestInfo):
+
+        if pullRequestInfo['merged'] is True:
+            return {'status':False, 'reason':'Pull request is already merged.'}
+
+        return {'status':True, 'reason':''}
 
     def git(self, command, arguments=[]):
 
@@ -77,32 +120,60 @@ class GitEngine:
 
     def invokeGitHubApi(self, url, params={}):
 
-        username = self.conf['github']['username']
-        password = self.conf['github']['password']
+        username = self.settings['github']['username']
+        password = self.settings['github']['password']
 
-        print "Invoking GitHub API '{}'".format(url)
+        print "\nInvoking GitHub API '{}'".format(url)
         response = requests.get(url, auth=(username, password))
         return response
+
+    def parsePullRequestUrl(self, pullRequestUrl):
+
+        matcher = re.search(self.settings['github']['urlPatternsRegEx']['pullRequest']['pattern'], pullRequestUrl)
+
+        groupConfig = self.settings['github']['urlPatternsRegEx']['pullRequest']['groups']
+
+        parsedPullRequest = {
+                             'owner':matcher.group(groupConfig['owner']),
+                             'repo':matcher.group(groupConfig['repo']),
+                             'pullRequestId':matcher.group(groupConfig['pullRequestId']),
+                            }
+
+        return parsedPullRequest
+
 
 def main(argv):
 
     parser = argparse.ArgumentParser(description='CI Tools.')
+    parser.add_argument('--settings', help='Settings file location')
 
     subparsers = parser.add_subparsers()
 
     mergePullRequest = subparsers.add_parser('mergePullRequest')
-    mergePullRequest.add_argument("pullRequest", help="Sample pull request => {'repo':'ci-tools', 'id':'1'}")
-    mergePullRequest.add_argument("--push", action="store_true", help="Flag to push the merge to the origin")
+    mergePullRequest.add_argument('pullRequest', help="e.g. => {'repoLocation':'ci-tools-test', 'pullRequestUrl':'https://github.com/john/ci-tools-test/pull/1'}")
+    mergePullRequest.add_argument('--push', action='store_true', help='Flag to push the merge to the origin')
 
     args = parser.parse_args()
 
-    data = json.loads(open(os.getcwd()+'/ci-tools-data.json').read())
-    repos = data['repositories']
-    conf = data['conf']
-    gitEngine = GitEngine(repos, conf);
+    settings = loadSettings(args.settings)
+
+    gitEngine = GitEngine(settings);
 
     if args.pullRequest is not None:
         gitEngine.mergePullRequest(eval(args.pullRequest), args.push);
 
-if __name__ == "__main__":
+def loadSettings(location):
+
+    if location is None:
+
+        defaultSettingsFileName = '/ci-tools-settings.json'
+        if os.path.exists(os.getcwd() + defaultSettingsFileName):
+            location = os.getcwd() + defaultSettingsFileName
+        else:
+            location = os.environ['HOME'] + '/.ci-tools'+ defaultSettingsFileName
+
+    return json.loads(open(location).read())
+
+
+if __name__ == '__main__':
    main(sys.argv[1:])
